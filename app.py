ef7fa510a832e2d80de5f91b1332040de2adec79
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2018-06-25 12:13:07 -0700

    Add template for inserting new word

--- app.py
+++ app.py
@@ -11,11 +11,21 @@ def index():
 @app.route('/get-random-word', methods=['GET'])
 def random_word():
 	# @param base_lang: base language in which this word should be
+	#
 	# return a random word (with meaning) as a dictionary
   return jsonify({
   	'word' : 'bonjour',
   	'meaning' : 'Good day!'
   })
+
+@app.route('/insert-word', methods=['GET'])
+def insert_word():
+	# @param word: the new word learned
+	# @param language: which language this word is in
+	# @param meaning: the meaning of this word
+	#
+	# insert a new word in the db
+	return
 
 if __name__ == "__main__":
 	app.run(debug=True)
