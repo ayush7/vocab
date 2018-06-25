db270794c37a07f1de0ae8ae74686240cef666c0
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2018-06-25 12:26:13 -0700

    Add util

---
+++ util.py
@@ -0,0 +1,19 @@
+# Utility functions
+ENV_FILENAME = '.env'
+
+def readenvline(line, dictionary):
+	# @param line: string of form 'key=value', no spaces
+	# Parse and insert line into 'dicionary'
+	try:
+		envvar = line.replace(' ', '').split('=')
+		dictionary[envvar[0]] = envvar[1]
+	except IndexError:
+		# incorrect format
+		print "Incorrect format for environment variable in line '{}'".format(line)
+
+def readenv():
+	# read env variables into dictionary and return
+	d = {}
+	with open(ENV_FILENAME, 'r') as f:
+		readenvline(f.readline(), d)
+	return d
